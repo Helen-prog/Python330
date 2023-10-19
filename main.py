4dc60ab198eda0d6c8403640d0bf971b8cb568b0
Helen <elena.kozovyakina@gmail.com>
2023-10-19 17:21:29 +0300

    Account

--- main.py
+++ main.py
@@ -3977,6 +3977,110 @@ import time
 # date3 = Date.from_string('21.12.2023')
 # print(date3.string_to_db())
 
+# class Account:
+#     rate_usd = 0.013  # 2
+#     rate_eur = 0.011  # 3
+#     suffix = 'RUB'
+#     suffix_usd = "USD"
+#     suffix_eur = "EUR"
+#
+#     def __init__(self, surname, num, percent, value):
+#         self.__num = num
+#         self.surname = surname
+#         self.percent = percent
+#         self.value = value
+#         print(f"Счет №{self.num} принадлежащий {self.surname} был открыт.")
+#         print("*" * 50)
+#
+#     def __del__(self):
+#         print("*" * 50)
+#         print(f"Счет №{self.num} принадлежащий {self.surname} был закрыт.")
+#
+#     @property
+#     def num(self):
+#         return self.__num
+#
+#     @num.setter
+#     def num(self, val):
+#         self.__num = val
+#
+#     @staticmethod
+#     def convert(value, rate):
+#         return value * rate
+#
+#     @classmethod
+#     def set_usd_rate(cls, rate):
+#         cls.rate_usd = rate
+#
+#     @classmethod
+#     def set_eur_rate(cls, rate):
+#         cls.rate_eur = rate
+#
+#     def convert_to_usd(self):
+#         usd_val = Account.convert(self.value, Account.rate_usd)
+#         print(f'Состояние счета: {usd_val} {Account.suffix_usd}')
+#
+#     def convert_to_eur(self):
+#         eur_val = Account.convert(self.value, Account.rate_eur)
+#         print(f'Состояние счета: {eur_val} {Account.suffix_eur}')
+#
+#     def edit_owner(self, surname):
+#         self.surname = surname
+#
+#     def add_percents(self):
+#         self.value += self.value * self.percent
+#         print("Проценты были успешно начислены")
+#         self.print_balance()
+#
+#     def withdraw_money(self, val):
+#         if val > self.value:
+#             print(f"К сожалению у вас нет {val} {Account.suffix}")
+#         else:
+#             self.value -= val
+#             print(f"{val} {Account.suffix} было успешно снято!")
+#         self.print_balance()
+#
+#     def add_money(self, val):
+#         self.value += val
+#         print(f"{val} {Account.suffix} было успешно добавлено!")
+#         self.print_balance()
+#
+#     def print_balance(self):
+#         print(f"Текущий баланс {self.value} {Account.suffix}")
+#
+#     def print_info(self):
+#         print('Информация о счете:')
+#         print('-' * 20)
+#         print(f"№{self.num}")
+#         print(f"Владелец: {self.surname}")
+#         self.print_balance()
+#         print(f"Проценты: {self.percent:.0%}")
+#         print('-' * 20)
+#
+#
+# acc = Account("Долгих", '12345', 0.03, 1000)
+# acc.print_info()
+# acc.convert_to_usd()
+# acc.convert_to_eur()
+# print()
+# Account.set_usd_rate(2)
+# acc.convert_to_usd()
+# Account.set_eur_rate(3)
+# acc.convert_to_eur()
+# print()
+# acc.edit_owner("Дюма")
+# acc.print_info()
+# acc.add_percents()
+# print()
+# acc.withdraw_money(3000)
+# print()
+# acc.withdraw_money(100)
+# print()
+# acc.add_money(5000)
+# print()
+# acc.withdraw_money(3000)
+# print()
+
 class Account:
     rate_usd = 0.013  # 2
     rate_eur = 0.011  # 3
@@ -3985,16 +4089,22 @@ class Account:
     suffix_eur = "EUR"
 
     def __init__(self, surname, num, percent, value):
-        self.num = num
+        self.__num = num
         self.surname = surname
         self.percent = percent
         self.value = value
-        print(f"Счет №{self.num} принадлежащий {self.surname} был открыт.")
+        print(f"Счет №{self.get_num()} принадлежащий {self.surname} был открыт.")
         print("*" * 50)
 
     def __del__(self):
         print("*" * 50)
-        print(f"Счет №{self.num} принадлежащий {self.surname} был закрыт.")
+        print(f"Счет №{self.__num} принадлежащий {self.surname} был закрыт.")
+
+    def get_num(self):
+        return self.__num
+
+    def set_num(self, val):
+        self.__num = val
 
     @staticmethod
     def convert(value, rate):
@@ -4043,7 +4153,7 @@ class Account:
     def print_info(self):
         print('Информация о счете:')
         print('-' * 20)
-        print(f"№{self.num}")
+        print(f"№{self.__num}")
         print(f"Владелец: {self.surname}")
         self.print_balance()
         print(f"Проценты: {self.percent:.0%}")
