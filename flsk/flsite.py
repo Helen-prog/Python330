9430f6c28b06d79288f999ac00fbb69e1c2e660f
Helen <elena.kozovyakina@gmail.com>
2024-01-16 22:21:02 +0300

    flask

--- flsk/flsite.py
+++ flsk/flsite.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, flash, session, redirect, url_for, g
+from flask import Flask, render_template, request, flash, session, redirect, url_for, g, abort
 import sqlite3
 import os
 from FDataBase import FDataBase
@@ -50,14 +50,36 @@ def close_db(error):
 def index():
     db = get_db()
     dbase = FDataBase(db)
-    return render_template('index.html', title="Главная", menu=dbase.get_menu())
+    return render_template('index.html', title="Главная", menu=dbase.get_menu(), posts=dbase.get_posts_anonce())
 
 
 @app.route("/add_post", methods=["POST", "GET"])
 def add_post():
     db = get_db()
     dbase = FDataBase(db)
+
+    if request.method == "POST":
+        if len(request.form['name']) > 4 and len(request.form['post']) > 10:
+            res = dbase.add_post(request.form['name'], request.form['post'], request.form['url'])
+            if not res:
+                flash("Ошибка добавления статьи", category="error")
+            else:
+                flash("Статья добавлена успешно", category="success")
+        else:
+            flash("Ошибка добавления статьи", category="error")
+
     return render_template('add_post.html', title="Добавление статьи", menu=dbase.get_menu())
+
+
+@app.route("/post/<alias>")
+def show_post(alias):
+    db = get_db()
+    dbase = FDataBase(db)
+    title, post = dbase.get_post(alias)
+    if not title:
+        abort(404)
+
+    return render_template('post.html', menu=dbase.get_menu(), title=title, post=post)
 
 
 @app.route("/about")
